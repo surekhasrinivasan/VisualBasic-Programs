d9bdb9ebe27b1aadc4b432bc85470a2eabeb590f
Surekhasrinivasan <surekhasrao@gmail.com>
2019-04-24 22:12:04 -0400

    Created AcceptDetails method

--- CalculateAreaOfRectangle/CalculateAreaOfRectangle/Module1.vb
+++ CalculateAreaOfRectangle/CalculateAreaOfRectangle/Module1.vb
@@ -5,6 +5,13 @@
         Private length As Double
         Private width As Double
 
+        'Public methods
+        Public Sub AcceptDetails()
+            length = 4.5
+            width = 5.5
+        End Sub
+
+
     End Class
     Sub Main()
         Dim r As New Rectangle()
