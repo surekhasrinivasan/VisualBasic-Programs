9187497776a53edfdd6d1f300b327f9f34b8681c
Surekhasrinivasan <surekhasrao@gmail.com>
2019-04-24 21:57:28 -0400

    Demo - DataTypes in VB completed

--- DataTypesInVBDemo/DataTypesInVBDemo/Module1.vb
+++ DataTypesInVBDemo/DataTypesInVBDemo/Module1.vb
@@ -1,6 +1,38 @@
 ﻿Module Module1
     'Demo of Datatypes in VB
     Sub Main()
+        Dim b As Byte
+        Dim n As Integer
+        Dim si As Single
+        Dim d As Double
+        Dim da As Date
+        Dim c As Char
+        Dim s As String
+        Dim bl As Boolean
+
+        b = 1
+        n = 1234567
+        si = 0.12345678901234566
+        d = 0.12345678901234559
+        da = Today
+        c = "U"c
+        s = "Me"
+
+        If ScriptEngine = "VB" Then
+            bl = True
+        Else
+            bl = False
+        End If
+
+        If bl Then
+            'the oath taking
+            Console.Write(c & " and, " & s & vbCrLf)
+            Console.WriteLine("declaring on the day of: {0}", da)
+            Console.WriteLine("We will learn VB.Net seriously")
+            Console.WriteLine("Lets see what happens to the floating point variables")
+            Console.WriteLine("The Single: {0}, The Double: {1}", si, d)
+        End If
+        Console.ReadKey()
 
     End Sub
 
