63b20b3c017dc5db1584fb1f54a2d532347424f5
Surekhasrinivasan <surekhasrao@gmail.com>
2019-04-25 22:25:04 -0400

    TypeConversion of boolean variable

--- TypeConversions/TypeConversions/DataTypes.vb
+++ TypeConversions/TypeConversions/DataTypes.vb
@@ -1,6 +1,15 @@
-﻿Module Module1
+﻿Module DataTypes
 
     Sub Main()
+        Dim n As Integer
+        Dim da As Date
+        Dim bl As Boolean = True
+        n = 1234567
+        da = Today
+
+        Console.WriteLine(bl)
+        Console.WriteLine(CSByte(bl))
+        Console.WriteLine(CStr(bl))
 
     End Sub
 
