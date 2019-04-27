35a7e7f343e8ba9fa6e06817a8867e9e345cc7a3
Surekhasrinivasan <surekhasrao@gmail.com>
2019-04-26 23:00:58 -0400

    VariableInitializationProgram - Adding two variables

--- VariableInitializationProgram/VariableInitializationProgram/variableNdatatypes.vb
+++ VariableInitializationProgram/VariableInitializationProgram/variableNdatatypes.vb
@@ -1,7 +1,15 @@
-﻿Module Module1
+﻿Module variableNdatatypes
 
     Sub Main()
+        Dim a As Short
+        Dim b As Integer
+        Dim c As Double
 
+        a = 10
+        b = 20
+        c = a + b
+        Console.WriteLine("a = {0}, b = {1}, c = {2}", a, b, c)
+        Console.ReadLine()
     End Sub
 
 End Module
