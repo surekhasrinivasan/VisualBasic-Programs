29ba4e3b7dcaba776ee44c5d1a0b2cea2bc59931
Surekhasrinivasan <surekhasrao@gmail.com>
2019-04-26 23:03:18 -0400

    VariableInitializationProgram - multiplying two variables

--- VariableInitializationProgram/VariableInitializationProgram/variableNdatatypes.vb
+++ VariableInitializationProgram/VariableInitializationProgram/variableNdatatypes.vb
@@ -4,11 +4,15 @@
         Dim a As Short
         Dim b As Integer
         Dim c As Double
+        Dim d As Double
 
         a = 10
         b = 20
         c = a + b
+        d = a * b
+
         Console.WriteLine("a = {0}, b = {1}, c = {2}", a, b, c)
+        Console.WriteLine("a = {0}, b = {1}, d = {2}", a, b, d)
         Console.ReadLine()
     End Sub
 
