e15f77bed1328ffa1ab190101d766b842d814f68
Surekhasrinivasan <surekhasrao@gmail.com>
2019-04-26 23:07:29 -0400

    VariableInitializationProgram - division of two variables

--- VariableInitializationProgram/VariableInitializationProgram/variableNdatatypes.vb
+++ VariableInitializationProgram/VariableInitializationProgram/variableNdatatypes.vb
@@ -6,16 +6,19 @@
         Dim c As Double
         Dim d As Double
         Dim e As Double
+        Dim f As Double
 
         a = 10
         b = 20
         c = a + b
         d = a * b
         e = a - b
+        f = a / b
 
         Console.WriteLine("a = {0}, b = {1}, c = {2}", a, b, c)
         Console.WriteLine("a = {0}, b = {1}, d = {2}", a, b, d)
         Console.WriteLine("a = {0}, b = {1}, c = {2}", a, b, e)
+        Console.WriteLine("a = {0}, b = {1}, c = {2}", a, b, f)
         Console.ReadLine()
     End Sub
 
