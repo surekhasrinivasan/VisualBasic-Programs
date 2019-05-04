26cca905807eba51916a7ee17523169f916cd31b
Surekhasrinivasan <surekhasrao@gmail.com>
2019-05-03 22:40:48 -0400

    Using constant value PI calculate the area - solved

--- UseOfConstValue/UseOfConstValue/constantNum.vb
+++ UseOfConstValue/UseOfConstValue/constantNum.vb
@@ -1,7 +1,12 @@
-﻿Module Module1
+﻿Module constantNum
 
     Sub Main()
-
+        Const PI = 3.14149
+        Dim radius, area As Single
+        radius = 7
+        area = PI * radius * radius
+        Console.WriteLine("Area = " & Str(area))
+        Console.Read()
     End Sub
 
 End Module
