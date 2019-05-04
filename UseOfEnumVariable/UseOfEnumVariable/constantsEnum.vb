7160c01683518aa0e54b139b52d893b89c51f06f
Surekhasrinivasan <surekhasrao@gmail.com>
2019-05-03 22:54:40 -0400

    UseOfEnumVariable - solved

--- UseOfEnumVariable/UseOfEnumVariable/constantsEnum.vb
+++ UseOfEnumVariable/UseOfEnumVariable/constantsEnum.vb
@@ -1,7 +1,20 @@
 ﻿Module constantsEnum
+    Enum Colors
+        red = 1
+        orange = 2
+        yellow = 3
+        green = 4
+        azure = 5
+        blue = 6
+        violet = 7
+    End Enum
 
     Sub Main()
-
+        Console.WriteLine("The color Red is: " & Colors.red)
+        Console.WriteLine("The color yellow is: " & Colors.yellow)
+        Console.WriteLine("The color blue is: " & Colors.blue)
+        Console.WriteLine("The color Green is: " & Colors.green)
+        Console.ReadLine()
     End Sub
 
 End Module
