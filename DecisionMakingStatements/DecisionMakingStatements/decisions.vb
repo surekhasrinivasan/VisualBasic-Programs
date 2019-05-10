a323d57dba4b93e88a575a96e079b3d26f7b4c4a
Surekhasrinivasan <surekhasrao@gmail.com>
2019-05-09 22:26:29 -0400

    Decision making statement demo program completed

--- DecisionMakingStatements/DecisionMakingStatements/decisions.vb
+++ DecisionMakingStatements/DecisionMakingStatements/decisions.vb
@@ -6,9 +6,11 @@
 
         'check the boolean condition using if statement'
         If (a < 20) Then
+            'if condition is true then print the following'
             Console.WriteLine("a is less than 20")
         End If
-
+        Console.WriteLine("value of a is : {0}", a)
+        Console.ReadLine()
     End Sub
 
 End Module
