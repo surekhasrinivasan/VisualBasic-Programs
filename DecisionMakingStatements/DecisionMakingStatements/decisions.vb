96c8ff6c91303a98ff26eb638d411d9cc8ca8048
Surekhasrinivasan <surekhasrao@gmail.com>
2019-05-09 22:21:45 -0400

    Check boolean condition using if statement

--- DecisionMakingStatements/DecisionMakingStatements/decisions.vb
+++ DecisionMakingStatements/DecisionMakingStatements/decisions.vb
@@ -1,6 +1,11 @@
 ﻿Module decisions
     'Program to demostrates a decision making statement
     Sub Main()
+        'local variable definition'
+        Dim a As Integer = 10
+
+        'check the boolean condition using if statement'
+
 
     End Sub
 
