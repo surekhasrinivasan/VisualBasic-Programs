86d5a123189344f33c7d9d1b3b87b2bb047a378f
Surekhasrinivasan <surekhasrao@gmail.com>
2019-05-10 22:37:44 -0400

    Create a string object by string literal

--- VBStringsProgram/VBStringsProgram/strings.vb
+++ VBStringsProgram/VBStringsProgram/strings.vb
@@ -1,7 +1,12 @@
 ﻿Module strings
     ' Program to demo strings in VB'
     Sub Main()
-
+        ' Creating string object by assigning a string literal to a String variable' 
+        Dim fname, lname, fullname As String
+        fname = "Rowan"
+        lname = "Atkinson"
+        fullname = fname + " " + lname
+        Console.WriteLine("Fullname : {0}", fullname)
     End Sub
 
 End Module
