b4916d2f4603e0df79a571b8bd132da2eecf89a1
Surekhasrinivasan <surekhasrao@gmail.com>
2019-05-10 22:42:08 -0400

    Create a string object by using String constructor

--- VBStringsProgram/VBStringsProgram/strings.vb
+++ VBStringsProgram/VBStringsProgram/strings.vb
@@ -2,11 +2,16 @@
     ' Program to demo strings in VB'
     Sub Main()
         ' Creating string object by assigning a string literal to a String variable' 
-        Dim fname, lname, fullname As String
+        Dim fname, lname, fullname, greetings As String
         fname = "Rowan"
         lname = "Atkinson"
         fullname = fname + " " + lname
         Console.WriteLine("Fullname : {0}", fullname)
+
+        'by using string constructor'
+        Dim letters As Char() = {"H", "e", "l", "l", "o"}
+        greetings = New String(letters)
+        Console.WriteLine("Greetings: {0}", greetings)
     End Sub
 
 End Module
