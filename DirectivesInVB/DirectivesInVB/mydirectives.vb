0933f9b37b67ba69a3173d12faa5eccc625d2922
Surekhasrinivasan <surekhasrao@gmail.com>
2019-05-14 21:42:09 -0400

    Demo for #Const directive

--- DirectivesInVB/DirectivesInVB/mydirectives.vb
+++ DirectivesInVB/DirectivesInVB/mydirectives.vb
@@ -1,7 +1,11 @@
 ﻿Module mydirectives
     'Example for the usage of #const directive'
+#Const age = True
     Sub Main()
-
+#If age Then
+        Console.WriteLine("Hello World! Welcome")
+#End If
+        Console.ReadKey()
     End Sub
 
 End Module
