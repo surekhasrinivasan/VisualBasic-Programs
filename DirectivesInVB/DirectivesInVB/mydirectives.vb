b07650c95f94b1bceede05cdb4232646815741e2
Surekhasrinivasan <surekhasrao@gmail.com>
2019-05-14 21:51:16 -0400

    Demo of #ExtermalSource directive

--- DirectivesInVB/DirectivesInVB/mydirectives.vb
+++ DirectivesInVB/DirectivesInVB/mydirectives.vb
@@ -1,11 +1,13 @@
 ﻿Module mydirectives
-    'Example for the usage of #const directive'
-#Const age = True
-    Sub Main()
-#If age Then
-        Console.WriteLine("Hello World! Welcome")
-#End If
-        Console.ReadKey()
-    End Sub
+    '    'Example for the usage of #const directive'
+    '#Const age = True
+    '    Sub Main()
+    '#If age Then
+    '        Console.WriteLine("Hello World! Welcome")
+    '#End If
+    '        Console.ReadKey()
+    '    End Sub
+
+    'Example for #ExternalSource directive'
 
 End Module
