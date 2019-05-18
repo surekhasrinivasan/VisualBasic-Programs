c3f5ff83cd106fa3f37b76eb2a3c6e21eb11e02d
Surekhasrinivasan <surekhasrao@gmail.com>
2019-05-17 23:51:52 -0400

    Creating string object by using string concantenation operator

--- CreatingStringObject/CreatingStringObject/Module1.vb
+++ CreatingStringObject/CreatingStringObject/Module1.vb
@@ -6,7 +6,11 @@
     ' By retrieving a Property Or calling a method that returns a String
     ' By calling a formatting method To convert a value Or Object To its String representation
     Sub Main()
-
+        Dim fname, lname, fullname, greetings As String
+        fname = "John"
+        lname = "Smith"
+        fullname = fname + " " + lname
+        Console.WriteLine("Full Name: {0}", fullname)
     End Sub
 
 End Module
