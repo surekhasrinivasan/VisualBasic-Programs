9fb1b5153df2b254732c4561a7b52fa9f56cde8f
Surekhasrinivasan <surekhasrao@gmail.com>
2019-05-18 12:48:57 -0400

    Using String.Compare() method two strings are compared

--- ComparingStrings/ComparingStrings/Module1.vb
+++ ComparingStrings/ComparingStrings/Module1.vb
@@ -1,7 +1,16 @@
 ﻿Module Module1
     ' Comparing Strings program
     Sub Main()
+        Dim str1, str2 As String
+        str1 = "This is Test"
+        str2 = "This is Text"
 
+        If (String.Compare(str1, str2) = 0) Then
+            Console.WriteLine(str1 + " and " + str2 + "are equal")
+        Else
+            Console.WriteLine(str1 + "and " + "are not equal")
+        End If
+        Console.ReadLine()
     End Sub
 
 End Module
