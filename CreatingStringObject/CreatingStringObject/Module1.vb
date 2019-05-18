3697726eed40515834d8f4337d42e3abff8983e4
Surekhasrinivasan <surekhasrao@gmail.com>
2019-05-17 23:49:46 -0400

    CreatingStringObject - initial commit

---
+++ CreatingStringObject/CreatingStringObject/Module1.vb
@@ -0,0 +1,12 @@
+﻿Module Module1
+    ' We can create String Object Using one Of the following methods −
+    ' By assigning a String literal To a String variable
+    ' By using a String class constructor
+    ' By using the string concatenation operator (+)
+    ' By retrieving a Property Or calling a method that returns a String
+    ' By calling a formatting method To convert a value Or Object To its String representation
+    Sub Main()
+
+    End Sub
+
+End Module
---
+++ CreatingStringObject/CreatingStringObject/My Project/AssemblyInfo.vb
@@ -0,0 +1,35 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+
+<Assembly: AssemblyTitle("CreatingStringObject")>
+<Assembly: AssemblyDescription("")>
+<Assembly: AssemblyCompany("HP Inc.")>
+<Assembly: AssemblyProduct("CreatingStringObject")>
+<Assembly: AssemblyCopyright("Copyright © HP Inc. 2019")>
+<Assembly: AssemblyTrademark("")>
+
+<Assembly: ComVisible(False)>
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("553ccc32-f52f-4779-bf30-3ab02acd7fb6")>
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version
+'      Build Number
+'      Revision
+'
+' You can specify all the values or you can default the Build and Revision Numbers
+' by using the '*' as shown below:
+' <Assembly: AssemblyVersion("1.0.*")>
+
+<Assembly: AssemblyVersion("1.0.0.0")>
+<Assembly: AssemblyFileVersion("1.0.0.0")>
