cd5100b75c628a3151a447b178e0a0d4507d1423
Surekhasrinivasan <surekhasrao@gmail.com>
2019-05-19 15:35:17 -0400

    GettingASubstring - completed

--- GettingASubstring/GettingASubstring/strings.vb
+++ GettingASubstring/GettingASubstring/strings.vb
@@ -1,7 +1,13 @@
 ﻿Module strings
     ' Program to demo Getting a substring
     Sub Main()
+        Dim str As String
+        str = "Last night I had dream of Mother Teresa"
+        Console.WriteLine(str)
 
+        Dim substr As String = str.Substring(26)
+        Console.WriteLine(substr)
+        Console.ReadLine()
     End Sub
 
 End Module
