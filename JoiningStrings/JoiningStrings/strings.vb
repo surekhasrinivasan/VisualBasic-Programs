943a234c72caf65d86f5fe5a6c8e7a0ff51a6620
Surekhasrinivasan <surekhasrao@gmail.com>
2019-05-19 15:32:23 -0400

    JoiningStrings - completed

--- JoiningStrings/JoiningStrings/strings.vb
+++ JoiningStrings/JoiningStrings/strings.vb
@@ -1,7 +1,15 @@
 ﻿Module strings
     ' Program to Join strings
     Sub Main()
+        Dim strArray As String() = {"Down the way where the nights are gay",
+         "And the sun shines daily on the mountain top",
+         "I took a trip on a sailing ship",
+         "And when I reached Jamaica",
+         "I made a stop"}
 
+        Dim str As String = String.Join(vbCrLf, strArray)
+        Console.WriteLine(str)
+        Console.ReadLine()
     End Sub
 
 End Module
