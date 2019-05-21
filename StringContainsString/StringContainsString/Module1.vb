5db6d659323e3dd77ae105ebf2b83484420af3aa
Surekhasrinivasan <surekhasrao@gmail.com>
2019-05-20 20:46:54 -0400

    StringContainsString - completed

--- StringContainsString/StringContainsString/Module1.vb
+++ StringContainsString/StringContainsString/Module1.vb
@@ -1,7 +1,13 @@
 ﻿Module Module1
     'Program to demo string contains string'
     Sub Main()
+        Dim str1 As String
+        str1 = "This is test"
 
+        If (str1.Contains("test")) Then
+            Console.WriteLine("The string 'test' was found in the string.")
+        End If
+        Console.ReadLine()
     End Sub
 
 End Module
