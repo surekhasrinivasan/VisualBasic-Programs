1700fa66f8210b7a68cd4063961e1aa17752b1e4
Surekhasrinivasan <surekhasrao@gmail.com>
2019-05-20 21:03:22 -0400

    CurrentDateAndTimeProgram - completed

--- CurrentDateAndTimeProgram/CurrentDateAndTimeProgram/dateNTime.vb
+++ CurrentDateAndTimeProgram/CurrentDateAndTimeProgram/dateNTime.vb
@@ -1,7 +1,10 @@
 ﻿Module dateNTime
     'Program to demonstrate how to get Current date
     Sub Main()
-
+        Console.WriteLine("Current Date: ")
+        Dim dt As Date = Today
+        Console.WriteLine("Today's date is {0}", dt)
+        Console.ReadLine()
     End Sub
 
 End Module
