35039e1179df25d3617ba90125c6301681ffe2b9
Surekhasrinivasan <surekhasrao@gmail.com>
2019-05-22 18:34:37 -0400

    displaying dates in different ways

--- CurrentDateAndTimeProgram/CurrentDateAndTimeProgram/dateNTime.vb
+++ CurrentDateAndTimeProgram/CurrentDateAndTimeProgram/dateNTime.vb
@@ -1,11 +1,13 @@
 ﻿Module dateNTime
     'Program to demonstrate how to get Current date
-    'Sub Main()
-    '    Console.WriteLine("Current Date: ")
-    '    Dim dt As Date = Today
-    '    Console.WriteLine("Today's date is {0}", dt)
-    '    Console.ReadLine()
-    'End Sub
+    Sub Main()
+        Console.WriteLine("Current Date: ")
+        Dim dt As Date = Today
+        Console.WriteLine("Today's date showing time: {0}", dt)
+        Console.WriteLine("Today'S date with day: {0}", dt.ToLongDateString)
+        Console.WriteLine("Today's date without showing time: {0}", dt.ToShortDateString)
+        Console.ReadLine()
+    End Sub
 
     ''Program to demonstrate how to get Current time
     'Sub Main()
@@ -15,21 +17,21 @@
     'End Sub
 
     'Program to demonstrate some of the properties and methods of DataAndTime class
-    Sub Main()
-        Dim birthday As Date
-        Dim bday As Integer
-        Dim month As Integer
-        Dim monthname As String
-        ' Assign a date using standard short format.
-        birthday = #8/27/2009#
-        bday = DateAndTime.Day(birthday)
-        month = DateAndTime.Month(birthday)
-        monthname = DateAndTime.MonthName(month)
+    'Sub Main()
+    '    Dim birthday As Date
+    '    Dim bday As Integer
+    '    Dim month As Integer
+    '    Dim monthname As String
+    '    ' Assign a date using standard short format.
+    '    birthday = #8/27/2009#
+    '    bday = DateAndTime.Day(birthday)
+    '    month = DateAndTime.Month(birthday)
+    '    monthname = DateAndTime.MonthName(month)
 
-        Console.WriteLine(birthday)
-        Console.WriteLine(bday)
-        Console.WriteLine(month)
-        Console.WriteLine(monthname)
-        Console.ReadLine()
-    End Sub
+    '    Console.WriteLine(birthday)
+    '    Console.WriteLine(bday)
+    '    Console.WriteLine(month)
+    '    Console.WriteLine(monthname)
+    '    Console.ReadLine()
+    'End Sub
 End Module
