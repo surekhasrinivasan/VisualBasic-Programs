45f06f91b2aef322d3cc7690ca378419c1e24876
Surekhasrinivasan <surekhasrao@gmail.com>
2019-05-23 21:12:18 -0400

    ArraysInVB - declare variables and initialize the array

--- ArraysInVB/ArraysInVB/arrayAp1.vb
+++ ArraysInVB/ArraysInVB/arrayAp1.vb
@@ -1,7 +1,11 @@
 ﻿Module arrayAp1
     ' Program to show the elements in an array can be stored and accessed by using the Index of the array
     Sub Main()
+        ' n is an array of 11 integers 
+        Dim n(10) As Integer
 
+        '  initialize elements of an array 'n'
+        Dim i, j As Integer
     End Sub
 
 End Module
