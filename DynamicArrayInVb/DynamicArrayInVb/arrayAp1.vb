ec19edfb3f04e385d6c4b8c50dc039f9295c3dd5
Surekhasrinivasan <surekhasrao@gmail.com>
2019-05-24 18:47:29 -0400

    declare array variables

--- DynamicArrayInVb/DynamicArrayInVb/arrayAp1.vb
+++ DynamicArrayInVb/DynamicArrayInVb/arrayAp1.vb
@@ -1,6 +1,14 @@
 ﻿Module arrayAp1
-    ' Program to demo dynamic array
+    ' Program to demo dynamic array. Dynamic arrays are arrays that can be dimensioned 
+    ' and re-dimensioned as par the need of the program. You can declare a dynamic 
+    ' array using the ReDim statement.
     Sub Main()
+        Dim marks() As Integer
+        ReDim marks(2)
+        marks(0) = 85
+        marks(1) = 75
+        marks(2) = 90
+
 
     End Sub
 
