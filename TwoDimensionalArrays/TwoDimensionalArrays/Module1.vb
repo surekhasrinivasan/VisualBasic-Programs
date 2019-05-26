42cbf3f972b53921550cdc5807c909322d9be9b3
Surekhasrinivasan <surekhasrao@gmail.com>
2019-05-26 07:14:02 -0400

    TwoDimensionalArrays -completed

--- TwoDimensionalArrays/TwoDimensionalArrays/Module1.vb
+++ TwoDimensionalArrays/TwoDimensionalArrays/Module1.vb
@@ -5,6 +5,13 @@
         Dim a(,) As Integer = {{0, 0}, {1, 2}, {2, 4}, {3, 6}, {4, 8}}
         Dim i, j As Integer
 
+        'Output each array element's values
+        For i = 0 To 4
+            For j = 0 To 1
+                Console.WriteLine("a[{0},{1}] = {2}", i, j, a(i, j))
+            Next j
+        Next i
+        Console.ReadLine()
     End Sub
 
 End Module
