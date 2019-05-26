4b6f678f10cd97fe88d3b53809ff0f1f26929fa3
Surekhasrinivasan <surekhasrao@gmail.com>
2019-05-26 07:08:12 -0400

    declare variables for two dimensional array

--- TwoDimensionalArrays/TwoDimensionalArrays/Module1.vb
+++ TwoDimensionalArrays/TwoDimensionalArrays/Module1.vb
@@ -1,6 +1,9 @@
 ﻿Module Module1
     ' Program to demo Two dimensional arrays 
     Sub Main()
+        ' An array with 5 rows and 2 columns'
+        Dim a(,) As Integer = {{0, 0}, {1, 2}, {2, 4}, {3, 6}, {4, 8}}
+        Dim i, j As Integer
 
     End Sub
 
