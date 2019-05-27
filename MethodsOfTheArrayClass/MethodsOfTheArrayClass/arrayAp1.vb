c5d2d0595a4764116bb950a0483f60cc4a366b4f
Surekhasrinivasan <surekhasrao@gmail.com>
2019-05-27 00:05:07 -0400

    display the original array elements

--- MethodsOfTheArrayClass/MethodsOfTheArrayClass/arrayAp1.vb
+++ MethodsOfTheArrayClass/MethodsOfTheArrayClass/arrayAp1.vb
@@ -1,7 +1,15 @@
 ﻿Module arrayAp1
     ' Program demonstrates use of some of the methods of the Array class
     Sub Main()
+        Dim list As Integer() = {34, 72, 13, 44, 25, 30, 10}
+        Dim temp As Integer() = list
+        Dim i As Integer
+        Console.Write("Original Array: ")
 
+        For Each i In list
+            Console.Write("{0} ", i)
+        Next i
+        Console.WriteLine()
     End Sub
 
 End Module
