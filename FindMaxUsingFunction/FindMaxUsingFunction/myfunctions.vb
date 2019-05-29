89f2601619aa0b86582f7e0d3837120ca4658567
Surekhasrinivasan <surekhasrao@gmail.com>
2019-05-28 21:23:43 -0400

    function FindMax implemented

--- FindMaxUsingFunction/FindMaxUsingFunction/myfunctions.vb
+++ FindMaxUsingFunction/FindMaxUsingFunction/myfunctions.vb
@@ -3,6 +3,12 @@
     Function FindMax(ByVal num1 As Integer, ByVal num2 As Integer) As Integer
         ' local variable declaration'
         Dim result As Integer
+
+        If (num1 > num2) Then
+            result = num1
+        Else
+            result = num2
+        End If
     End Function
 
     Sub Main()
