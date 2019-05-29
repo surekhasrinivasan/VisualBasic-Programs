b48c01dcca338aa03a82852a47cdf22c5db82bf9
Surekhasrinivasan <surekhasrao@gmail.com>
2019-05-28 21:47:35 -0400

    FindMaxUsingFunction - completed

--- FindMaxUsingFunction/FindMaxUsingFunction/myfunctions.vb
+++ FindMaxUsingFunction/FindMaxUsingFunction/myfunctions.vb
@@ -9,10 +9,16 @@
         Else
             result = num2
         End If
+        FindMax = result
     End Function
-
     Sub Main()
+        Dim a As Integer = 100
+        Dim b As Integer = 200
+        Dim res As Integer
 
+        res = FindMax(a, b)
+
+        Console.WriteLine("Max value is: {0}", res)
+        Console.ReadLine()
     End Sub
-
 End Module
