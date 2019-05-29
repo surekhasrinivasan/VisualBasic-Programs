7d6ce7445b130e4b8bedc4a117e0f4afe4b961f7
Surekhasrinivasan <surekhasrao@gmail.com>
2019-05-28 21:15:41 -0400

    function declaration

--- FindMaxUsingFunction/FindMaxUsingFunction/myfunctions.vb
+++ FindMaxUsingFunction/FindMaxUsingFunction/myfunctions.vb
@@ -1,5 +1,10 @@
 ﻿Module myfunctions
     ' Program to find maximum number of two numbers using function
+    Function FindMax(ByVal num1 As Integer, ByVal num2 As Integer) As Integer
+        ' local variable declaration'
+        Dim result As Integer
+    End Function
+
     Sub Main()
 
     End Sub
