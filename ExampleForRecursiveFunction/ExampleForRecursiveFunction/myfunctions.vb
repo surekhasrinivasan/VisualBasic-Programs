1b2b39babe72d18a7035305d8c343e407f1e11b7
Surekhasrinivasan <surekhasrao@gmail.com>
2019-05-29 20:20:12 -0400

    function and local variable declaration

--- ExampleForRecursiveFunction/ExampleForRecursiveFunction/myfunctions.vb
+++ ExampleForRecursiveFunction/ExampleForRecursiveFunction/myfunctions.vb
@@ -3,6 +3,11 @@
     ' A function can call itself. This is known as recursion. 
     ' Following Is an example that calculates factorial for a given number using a recursive function
 
+    Function factorial(ByVal num As Integer) As Integer
+        ' local variable declaration 
+        Dim result As Integer
+
+    End Function
     Sub Main()
 
     End Sub
