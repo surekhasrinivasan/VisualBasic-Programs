68ccc703a7c27c820dc8b8ee4d80729b4046fc29
Surekhasrinivasan <surekhasrao@gmail.com>
2019-05-30 19:59:23 -0400

    ParamArrayFunction - initial commit

---
+++ ParamArrayFunction/ParamArrayFunction/Module1.vb
@@ -0,0 +1,10 @@
+﻿Module Module1
+    ' Program to demo ParamArray. ParamArray allows you to pass an arbitrary number of arguments 
+    ' to the procedure. A ParamArray parameter is always declared using ByVal. 
+    ' You can supply one Or more arguments To a ParamArray parameter by passing an array Of the 
+    ' appropriate data type, a comma-separated list Of values, Or Nothing at all.
+    Sub Main()
+
+    End Sub
+
+End Module
