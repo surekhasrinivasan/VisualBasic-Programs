39fd87e59ad93a11c99ce064b4766782bde0e124
Surekhasrinivasan <surekhasrao@gmail.com>
2019-05-29 20:18:03 -0400

    ExampleForRecursiveFunction - initial commit

---
+++ ExampleForRecursiveFunction/ExampleForRecursiveFunction/myfunctions.vb
@@ -0,0 +1,10 @@
+﻿Module myfunctions
+    ' Program to demo recursive function. 
+    ' A function can call itself. This is known as recursion. 
+    ' Following Is an example that calculates factorial for a given number using a recursive function
+
+    Sub Main()
+
+    End Sub
+
+End Module
