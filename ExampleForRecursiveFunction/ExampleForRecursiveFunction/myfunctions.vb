f82b0bdae40827ec7338ecd72e9d603098514911
Surekhasrinivasan <surekhasrao@gmail.com>
2019-05-29 20:31:06 -0400

    ExampleForRecursiveFunction - completed

--- ExampleForRecursiveFunction/ExampleForRecursiveFunction/myfunctions.vb
+++ ExampleForRecursiveFunction/ExampleForRecursiveFunction/myfunctions.vb
@@ -15,7 +15,11 @@
         End If
     End Function
     Sub Main()
-
+        ' calling the factorial method
+        Console.WriteLine("Factorial of 6 is : {0}", factorial(5))
+        Console.WriteLine("Factorial of 6 is : {0}", factorial(6))
+        Console.WriteLine("Factorial of 6 is : {0}", factorial(7))
+        Console.WriteLine("Factorial of 6 is : {0}", factorial(8))
+        Console.ReadLine()
     End Sub
-
 End Module
