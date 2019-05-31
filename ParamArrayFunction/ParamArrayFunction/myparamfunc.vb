d0ed3093ace855fb4aac12c0a9c845dfc770127f
Surekhasrinivasan <surekhasrao@gmail.com>
2019-05-30 20:22:33 -0400

    function and variable declaration

--- ParamArrayFunction/ParamArrayFunction/myparamfunc.vb
+++ ParamArrayFunction/ParamArrayFunction/myparamfunc.vb
@@ -1,8 +1,15 @@
-﻿Module Module1
+﻿Module myparamfunc
     ' Program to demo ParamArray. ParamArray allows you to pass an arbitrary number of arguments 
     ' to the procedure. A ParamArray parameter is always declared using ByVal. 
     ' You can supply one Or more arguments To a ParamArray parameter by passing an array Of the 
     ' appropriate data type, a comma-separated list Of values, Or Nothing at all.
+
+    ' Function to add elements of an array
+    Function AddElements(ParamArray arr As Integer()) As Integer
+        Dim sum As Integer = 0
+        Dim i As Integer = 0
+
+    End Function
     Sub Main()
 
     End Sub
