a460486ea18467e7fc89fbc70523f5e9d5d8f20a
Surekhasrinivasan <surekhasrao@gmail.com>
2019-05-31 22:40:38 -0400

    Function and local variables declaration

--- PassingArraysAsFunction/PassingArraysAsFunction/Module1.vb
+++
@@ -1,8 +0,0 @@
-﻿Module Module1
-    ' Program to demo passing array as function 
-
-    Sub Main()
-
-    End Sub
-
-End Module
---
+++ PassingArraysAsFunction/PassingArraysAsFunction/arrayParameter.vb
@@ -0,0 +1,14 @@
+﻿Module arrayParameter
+    ' Program to demo passing array as function 
+    Function getAverage(ByVal arr As Integer(), ByVal size As Integer) As Double
+        ' local variables 
+        Dim i As Integer
+        Dim avg As Double
+        Dim sum As Integer = 0
+
+    End Function
+    Sub Main()
+
+    End Sub
+
+End Module
