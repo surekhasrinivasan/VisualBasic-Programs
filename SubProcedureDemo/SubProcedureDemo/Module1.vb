c60d6776151070c053db044f2cc68e4a097e0610
Surekhasrinivasan <surekhasrao@gmail.com>
2019-06-01 22:43:40 -0400

    SubProcedureDemo - initial commit

---
+++ SubProcedureDemo/SubProcedureDemo/Module1.vb
@@ -0,0 +1,9 @@
+﻿Module Module1
+    ' Sub procedures are procedures that do not return any value. 
+
+    ' Program demonstrates a Sub procedure CalculatePay that takes two parameters hours and wages and displays the total pay of an employee
+    Sub Main()
+
+    End Sub
+
+End Module
