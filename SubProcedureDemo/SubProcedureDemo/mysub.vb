aa3e6bf9862a7d8014f83d78206e004d0ada0290
Surekhasrinivasan <surekhasrao@gmail.com>
2019-06-01 22:53:40 -0400

    Sub procedure and variable declaration

--- SubProcedureDemo/SubProcedureDemo/mysub.vb
+++ SubProcedureDemo/SubProcedureDemo/mysub.vb
@@ -1,7 +1,12 @@
-﻿Module Module1
+﻿Module mysub
     ' Sub procedures are procedures that do not return any value. 
 
     ' Program demonstrates a Sub procedure CalculatePay that takes two parameters hours and wages and displays the total pay of an employee
+    Sub CalculatePay(ByRef hours As Double, ByRef wage As Decimal)
+        ' local variable declaration
+        Dim pay As Double
+
+    End Sub
     Sub Main()
 
     End Sub
