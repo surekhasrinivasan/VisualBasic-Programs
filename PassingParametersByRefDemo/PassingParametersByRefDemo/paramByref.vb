78a36e51f20771bfdf539f6248a1614f7e39703e
Surekhasrinivasan <surekhasrao@gmail.com>
2019-06-03 18:54:20 -0400

    PassingParametersByRefDemo - initial commit

---
+++ PassingParametersByRefDemo/PassingParametersByRefDemo/paramByref.vb
@@ -0,0 +1,12 @@
+﻿Module paramByref
+    ' Program to demo Passing Parameters by Ref
+
+    ' A reference parameter is a reference to a memory location of a variable. 
+    ' When you pass parameters by reference, unlike value parameters, a new storage location is 
+    ' Not created for these parameters. The reference parameters represent the same memory location 
+    ' as the actual parameters that are supplied to the method.
+    Sub Main()
+
+    End Sub
+
+End Module
