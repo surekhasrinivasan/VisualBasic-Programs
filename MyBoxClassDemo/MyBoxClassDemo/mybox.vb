560ca41d36270a9fcad1a6d78000970f918a658f
Surekhasrinivasan <surekhasrao@gmail.com>
2019-06-04 21:15:52 -0400

    declare three data members of Box class

--- MyBoxClassDemo/MyBoxClassDemo/mybox.vb
+++ MyBoxClassDemo/MyBoxClassDemo/mybox.vb
@@ -1,6 +1,11 @@
 ﻿Module mybox
 
     ' Program that demonstrates a example for class like Box class, with three data members, length, breadth and height
+    Class Box
+        Public length As Double ' Length of a box
+        Public breadth As Double 'Breadth of a box
+        Public height As Double ' Height of a box 
+    End Class
     Sub Main()
 
     End Sub
