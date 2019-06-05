52121fab801e9c4a0851fa73e04f889eb0c09adb
Surekhasrinivasan <surekhasrao@gmail.com>
2019-06-04 21:32:46 -0400

    Box1 object instantiated and volume of the box1 is calculated

--- MyBoxClassDemo/MyBoxClassDemo/mybox.vb
+++ MyBoxClassDemo/MyBoxClassDemo/mybox.vb
@@ -11,7 +11,20 @@
         Dim Box2 As Box = New Box() ' Declare Box2 of type Box
         Dim volume As Double = 0.0  ' Store the volume of a box here
 
+        'box1 specification
+        Box1.height = 5.0
+        Box1.length = 6.0
+        Box1.breadth = 7.0
 
+        'box2 specification
+        Box2.height = 10.0
+        Box2.length = 12.0
+        Box2.breadth = 13.0
+
+        'volume of box1
+        volume = Box1.height * Box1.length * Box1.breadth
+        Console.WriteLine("Volume of Box1: {0}", volume)
+        Console.ReadKey()
     End Sub
 
 End Module
