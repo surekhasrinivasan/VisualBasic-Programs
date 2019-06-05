2f3c143b65dd806544b75a0be103fd6fa4a3b414
Surekhasrinivasan <surekhasrao@gmail.com>
2019-06-04 21:25:18 -0400

    declare variables in the main function

--- MyBoxClassDemo/MyBoxClassDemo/mybox.vb
+++ MyBoxClassDemo/MyBoxClassDemo/mybox.vb
@@ -7,6 +7,10 @@
         Public height As Double ' Height of a box 
     End Class
     Sub Main()
+        Dim Box1 As Box = New Box() ' Declare Box1 of type Box
+        Dim Box2 As Box = New Box() ' Declare Box2 of type Box
+        Dim volume As Double = 0.0  ' Store the volume of a box here
+
 
     End Sub
 
