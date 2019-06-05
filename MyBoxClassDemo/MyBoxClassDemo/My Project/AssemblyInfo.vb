86114121a03564c97ecec25dde6936fa55777019
Surekhasrinivasan <surekhasrao@gmail.com>
2019-06-04 20:29:42 -0400

    Example for class - initial commit

---
+++ MyBoxClassDemo/MyBoxClassDemo/My Project/AssemblyInfo.vb
@@ -0,0 +1,35 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+
+<Assembly: AssemblyTitle("MyBoxClassDemo")>
+<Assembly: AssemblyDescription("")>
+<Assembly: AssemblyCompany("HP Inc.")>
+<Assembly: AssemblyProduct("MyBoxClassDemo")>
+<Assembly: AssemblyCopyright("Copyright © HP Inc. 2019")>
+<Assembly: AssemblyTrademark("")>
+
+<Assembly: ComVisible(False)>
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("fd954222-6534-498d-8f4c-a9bd4a373f0a")>
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version
+'      Build Number
+'      Revision
+'
+' You can specify all the values or you can default the Build and Revision Numbers
+' by using the '*' as shown below:
+' <Assembly: AssemblyVersion("1.0.*")>
+
+<Assembly: AssemblyVersion("1.0.0.0")>
+<Assembly: AssemblyFileVersion("1.0.0.0")>
---
+++ MyBoxClassDemo/MyBoxClassDemo/mybox.vb
@@ -0,0 +1,8 @@
+﻿Module mybox
+
+    ' Program that demonstrates a example for class like Box class, with three data members, length, breadth and height
+    Sub Main()
+
+    End Sub
+
+End Module
