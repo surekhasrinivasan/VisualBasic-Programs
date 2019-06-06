8ef182e2db18a97fbf4e338d6932a52d87b5f2c9
Surekhasrinivasan <surekhasrao@gmail.com>
2019-06-06 19:34:43 -0400

    ConstructorDemo - initial commit

---
+++ ConstructorDemo/ConstructorDemo/Module1.vb
@@ -0,0 +1,10 @@
+﻿Module Module1
+    ' Program to demo Constructor
+
+    ' A class constructor is a special member Sub of a class that is executed whenever we create new objects 
+    ' of that class. A constructor has the name New and it does not have any return type.
+    Sub Main()
+
+    End Sub
+
+End Module
