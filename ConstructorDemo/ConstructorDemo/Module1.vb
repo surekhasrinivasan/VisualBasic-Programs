42850b3c03f3e9f19b52b8beb0d081e775ae8ad9
Surekhasrinivasan <surekhasrao@gmail.com>
2019-06-06 19:41:03 -0400

    declare data member and create constructor

--- ConstructorDemo/ConstructorDemo/Module1.vb
+++ ConstructorDemo/ConstructorDemo/Module1.vb
@@ -3,6 +3,12 @@
 
     ' A class constructor is a special member Sub of a class that is executed whenever we create new objects 
     ' of that class. A constructor has the name New and it does not have any return type.
+    Class Line
+        Private length As Double    ' length of a line
+        Public Sub New() ' constructor
+            Console.WriteLine("Object is being created.")
+        End Sub
+    End Class
     Sub Main()
 
     End Sub
