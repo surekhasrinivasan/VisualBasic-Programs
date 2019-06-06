9686bcfb392d0576b2b9b99a1b9aca27c33d5920
Surekhasrinivasan <surekhasrao@gmail.com>
2019-06-05 22:01:42 -0400

    Usage of set and get functions - program completed

--- MyBoxClassDemo/MyBoxClassDemo/mybox.vb
+++ MyBoxClassDemo/MyBoxClassDemo/mybox.vb
@@ -24,10 +24,31 @@
         Public Function getVolume() As Double
             Return length * breadth * height
         End Function
-
     End Class
     Sub Main()
+        Dim Box1 As Box = New Box() ' Declare Box1 of type Box
+        Dim Box2 As Box = New Box() ' Declare Box2 of type Box
+        Dim volume As Double = 0.0  ' Store the volume of a box here
 
+        'box1 specification
+        Box1.setLength(6.0)
+        Box1.setBreadth(7.0)
+        Box1.setHeight(5.0)
+
+        'box2 specification
+        Box2.setLength(12.0)
+        Box2.setBreadth(13.0)
+        Box2.setHeight(10.0)
+
+        'volume of box1
+        volume = Box1.getVolume()
+        Console.WriteLine("Volume of Box1: {0}", volume)
+
+        'volume of box2
+        volume = Box2.getVolume()
+        Console.WriteLine("Volume of Box2: {0}", volume)
+
+        Console.ReadKey()
     End Sub
 
 End Module
