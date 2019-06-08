4981e262884035122b0533144d62aba7ec47c9a7
Surekhasrinivasan <surekhasrao@gmail.com>
2019-06-07 22:20:53 -0400

    ParameterizedConstructorDemo - initial commit

---
+++ ParameterizedConstructorDemo/ParameterizedConstructorDemo/Module1.vb
@@ -0,0 +1,9 @@
+﻿Module Module1
+    ' A default constructor does not have any parameter, but if you need, a constructor can have parameters. 
+    ' Such constructors are called parameterized constructors. This technique helps you To assign initial 
+    ' value To an Object at the time Of its creation As shown In the following example 
+    Sub Main()
+
+    End Sub
+
+End Module
---
+++ ParameterizedConstructorDemo/ParameterizedConstructorDemo/My Project/AssemblyInfo.vb
@@ -0,0 +1,35 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+
+<Assembly: AssemblyTitle("ParameterizedConstructorDemo")>
+<Assembly: AssemblyDescription("")>
+<Assembly: AssemblyCompany("HP Inc.")>
+<Assembly: AssemblyProduct("ParameterizedConstructorDemo")>
+<Assembly: AssemblyCopyright("Copyright © HP Inc. 2019")>
+<Assembly: AssemblyTrademark("")>
+
+<Assembly: ComVisible(False)>
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("e6e12b0a-e883-44d2-84d9-7aa6814aa417")>
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version
+'      Build Number
+'      Revision
+'
+' You can specify all the values or you can default the Build and Revision Numbers
+' by using the '*' as shown below:
+' <Assembly: AssemblyVersion("1.0.*")>
+
+<Assembly: AssemblyVersion("1.0.0.0")>
+<Assembly: AssemblyFileVersion("1.0.0.0")>
