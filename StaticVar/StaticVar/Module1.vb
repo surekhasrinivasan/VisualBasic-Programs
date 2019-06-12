55252d9d8da60357c94821dc39ab703c303313fc
Surekhasrinivasan <surekhasrao@gmail.com>
2019-06-11 21:57:31 -0400

    Implementation of Shared function

--- StaticVar/StaticVar/Module1.vb
+++ StaticVar/StaticVar/Module1.vb
@@ -3,6 +3,12 @@
     Class StaticVar
         Public Shared num As Integer
     End Class
-
-
+    Public Sub count()
+        num = num + 1
+    End Sub
+    Public Shared Function getNum() As Integer
+        Return num
+    End Function
 End Module
+
+
